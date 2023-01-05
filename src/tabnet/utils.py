ffd4f3040cecff54105a2f6cc72114fb32ba855a
Koki Yamada <nino02000@icloud.com>
2023-01-05 06:51:05 +0000

    update load_model func

--- src/tabnet/utils.py
+++ src/tabnet/utils.py
@@ -1,5 +1,7 @@
 import copy
 from typing import List
+import zipfile
+import io
 
 import torch
 from torch import nn, Tensor
@@ -11,7 +13,23 @@ def load_weights_from_pretrained(
     update_state_dict = copy.deepcopy(model.state_dict())
 
     if file_path is not None:
-        pretrained_model.load_state_dict(torch.load(file_path))
+        if file_path.endswith(".zip"):
+            try:
+                with zipfile.ZipFile(file_path) as z:
+                    with z.open("network.pt") as f:
+                        try:
+                            saved_state_dict = torch.load(f)
+                        except io.UnsupportedOperation:
+                            saved_state_dict = torch.load(io.BytesIO(f.read()))
+            except KeyError:
+                raise KeyError("Your zip file is missing at least one component")
+        elif file_path.endswith(".pth"):
+            saved_state_dict = torch.load(file_path)
+        else:
+            raise ValueError("File extension must be .zip or .pth")
+
+        # load model parameters
+        pretrained_model.load_state_dict(saved_state_dict)
 
     for param, weights in pretrained_model.state_dict().items():
         if param.startswith("encoder"):
