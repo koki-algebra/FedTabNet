21740fc1b4f004a82e23b9e4c3630fb051831f3b
KokiYamada <nino02000@icloud.com>
2023-01-13 12:38:20 +0000

    add test

--- src/tabnet/client_pseudo_labeling.py
+++ src/tabnet/client_pseudo_labeling.py
@@ -1,6 +1,5 @@
 import time
 import logging
-import math
 
 import torch
 from torch import Tensor
@@ -104,4 +103,42 @@ class PseudoLabelingClient(BaseClient):
         logger.info("Client {}, Train Time: {}".format(self.cid, self.train_time))
 
     def test(self, conf, device):
-        pass
+        begin_test_time = time.time()
+        self.model.eval()
+        self.model.to(device)
+
+        loss_fn = self.load_loss_fn(conf)
+
+        if self.test_loader is None:
+            self.test_loader = self.test_data.loader(
+                conf.test_batch_size, self.cid, shuffle=False, seed=conf.seed
+            )
+
+        self.test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for X, y in self.test_loader:
+                X: Tensor = X.to(device).float()
+                y: Tensor = y.to(device).long()
+
+                # predict
+                log_probs, _ = self.model(X)
+
+                # loss
+                loss = loss_fn(log_probs, y)
+                _, y_pred = torch.max(log_probs, -1)
+                correct += y_pred.eq(y.data.view_as(y_pred)).long().cpu().sum()
+                self.test_loss += loss.item()
+
+            test_size = self.test_data.size(self.cid)
+            self.test_loss /= test_size
+            self.test_accuracy = 100.0 * float(correct) / test_size
+
+        logger.info(
+            "Client {}, testing -- Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)".format(
+                self.cid, self.test_loss, correct, test_size, self.test_accuracy
+            )
+        )
+
+        self.test_time = time.time() - begin_test_time
+        self.model = self.model.cpu()
